48f976a652bdb312121ebe32dcd608389417b7ab
Eric Chrobak <eric@ericchrobak.com>
2018-09-01 03:18:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,29 @@
+# class Application
+#   def call(env)
+#     resp = Rack::Response.new
+#     now = Time.now
+#     nowHr = now.hour
+#     if nowHr < 12 && nowHr >= 5
+#       resp.write "Good Morning"
+#     elsif nowHr >= 12 && nowHr < 17
+#       resp.write "Good Afternoon"
+#     end
+#     resp.finish
+#   end
+# end
+
 class Application
+
+  @@items = ["Apples","Carrots","Pears"]
+
   def call(env)
     resp = Rack::Response.new
-    now = Time.now
-    nowHr = now.hour
-    if nowHr < 12 && nowHr >= 5
-      resp.write "Good Morning"
-    elsif nowHr >= 12 && nowHr < 17
-      resp.write "Good Afternoon"
+    req = Rack::Request.new(env)
+
+    @@items.each do |item|
+      resp.write "#{item}\n"
     end
+
     resp.finish
   end
 end
